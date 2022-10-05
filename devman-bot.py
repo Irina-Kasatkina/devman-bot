50f391a1b152b069bac164494d48a527bf24a14a
Irina Kasatkina <kasatkina235@gmail.com>
2022-10-05 19:09:31 +0300

    initial commit

---
+++ devman-bot.py
@@ -0,0 +1,73 @@
+# coding=utf-8
+
+"""Отслеживает проверку работ на dvmn.org и информирует пользователя в telegram-чате."""
+
+import json
+import os
+import time
+
+import requests
+import telegram
+import telegram.error
+from dotenv import load_dotenv
+
+
+LONG_POLLING_TIMEOUT = 100
+
+
+def monitor_devman_attempts(devman_api_token, telegram_bot, telegram_chat_id):
+    """Отслеживает проверку работ на dvmn.org и информирует пользователя в telegram-чате."""
+
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {'Authorization': f'Token {devman_api_token}'}
+    timestamp = 0
+    params = {}
+    delay = 1
+    while True:
+        try:
+            if timestamp:
+                params = {'timestamp': timestamp}
+
+            response = requests.get(url, headers=headers, params=params, timeout=LONG_POLLING_TIMEOUT)
+            response.raise_for_status()
+
+            response_payload = response.json()
+            if response_payload['status'] == 'timeout':
+                timestamp = int(response_payload['timestamp_to_request'])
+            elif response_payload['status'] == 'found':
+                for attempt in response_payload['new_attempts']:
+                    message = generate_message_on_attempt(attempt)
+                    telegram_bot.send_message(chat_id=telegram_chat_id, 
+                                              text=message, 
+                                              parse_mode=telegram.constants.PARSEMODE_HTML)
+        except requests.exceptions.ReadTimeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            time.sleep(delay)
+            delay = 10
+
+
+def generate_message_on_attempt(attempt):
+    """Формирует текст сообщения для telegram-бота."""
+
+    message = (f'У вас проверили работу "<a href="{attempt["lesson_url"]}">'
+               f'{attempt["lesson_title"]}</a>"\n\n')
+    if attempt['is_negative']:
+        message = f'{message}К сожалению, в работе нашлись ошибки.\n'
+    else:
+        message = f'{message}Преподавателю всё понравилось, можно приступать к следующему уроку!\n'
+    return message
+
+
+def main():
+    load_dotenv()
+    devman_api_token = os.environ['DEVMAN_API_TOKEN']
+    telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+    telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
+
+    telegram_bot = telegram.Bot(token=telegram_bot_token)
+    monitor_devman_attempts(devman_api_token, telegram_bot, telegram_chat_id)
+
+
+if __name__ == '__main__':
+    main()
